f0bb75d472bfafe167f5c3228dbafb59bcac5668
Itay Feldman <itay1405@gmail.com>
2018-12-12 23:16:26 +0200

    Added helpful notes for encryption stages
    Signed-off-by: Itay Feldman <itay1405@gmail.com>

---
+++ RSA.py
@@ -0,0 +1,5 @@
+# Primes: p, q | N = p*q | phi(N) = coprimes = (p-1)*(q-1) | e: 1 < e < phi(N), e is coprime of N AND phi(N) |
+# d where: d*e (mod phi(N)) = 1
+# Lock: (e, N), Key: (d, N)
+# Encrypt given decrypted message M and encrypted message S: M^e (mod N) = S
+# Decrypt given encrypted message S and decrypted message M: S^d (mod N) = M
